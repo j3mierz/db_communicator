991a233183a48ebb32319c9a3440dafb698785ce
Julian Mierzejewski <j3mierz@gmail.com>
2024-04-19 12:05:15 +0200

    getting users by username from datebase/users  method works

--- models.py
+++ models.py
@@ -18,12 +18,23 @@ class Users:
     def new_user_to_db(self):
         conn = connect_db()
         cur = conn.cursor()
-        cur.execute("INSERT INTO users(username, password) VALUES (%s, %s) RETURNING id", (self.username, self._password))
+        cur.execute("INSERT INTO users(username, password) VALUES (%s, %s) RETURNING id",
+                    (self.username, self._password))
         self._id = cur.fetchone()[0]
         conn.commit()
+        cur.close()
         conn.close()
 
+    def load_user_by_username(self, username):
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute(f"select * from users where username like '{username}'")
+        result = cur.fetchall()
+        conn.commit()
+        cur.close()
+        conn.close()
+        return result
 
-a = Users("Paweu", "123456")
 
-
+a = Users()
+print(a.load_user_by_username('Paweu'))
